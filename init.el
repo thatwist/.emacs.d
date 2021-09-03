8b6035ccc373f2a321596dd1ec94d16fb68aeebe
Yurii Ostapchuk <twist522@gmail.com>
2021-09-02 23:16:59 -0500

    finish restructure

--- init.el
+++ init.el
@@ -1,2 +1,4 @@
-(require 'org)
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+; dunwanna load org that early
+;(require 'org)
+;(org-babel-load-file (concat user-emacs-directory "config.org"))
+(load-file (concat user-emacs-directory "config.el"))
