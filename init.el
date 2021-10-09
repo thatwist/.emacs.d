a375b23fcda8cf45197b87820f136c1079a5f363
Yuri Ostapchuk <twist522@gmail.com>
2021-10-09 09:39:18 -0500

    major

--- init.el
+++ init.el
@@ -1,4 +1,1 @@
-; dunwanna load org that early
-;(require 'org)
-;(org-babel-load-file (concat user-emacs-directory "config.org"))
-(load-file (concat user-emacs-directory "config.el"))
+(load-file (concat user-emacs-directory "config.elc"))
