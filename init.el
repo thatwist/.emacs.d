e8003fb3566c7244fc8bec124e04acbe2639a2e6
Yurii Ostapchuk <twist522@gmail.com>
2021-10-15 14:49:41 -0600

    'ensure' fix

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
-(load "config")
-;(native-elisp-load "config.elc")
+;; todo - smarter early-init
+;;(push (concat user-emacs-directory "lisp") load-path)
+(if (and (fboundp 'native-comp-available-p) (native-comp-available-p))
+    (progn (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))))
+;;(load-library "config")
+;; todo - handle nil
+(native-elisp-load (comp-lookup-eln "lisp/config.el"))
+;;(require 'config)
+;;(native-elisp-load "config.elc")
 
